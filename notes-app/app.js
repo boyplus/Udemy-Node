1a0feda2d945b59a7fd8628193e26bc088def141
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-06-28 21:13:17 +0700

    [ADD] yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,41 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-console.log(getNotes());
+yargs.version('1.1.0');
+
+//create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Add a new note');
+    },
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Remove a note');
+    },
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing out all the notes',
+    handler: function () {
+        console.log('Listing out all the notes');
+    },
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Read a note');
+    },
+});
+
+console.log(yargs.argv);
