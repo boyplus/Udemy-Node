20b2f7d2283d18db6b0cb14268b785dc2ff3ee44
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-07-31 16:21:06 +0700

    [UPDATE] task route

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -54,19 +54,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id);
-        if (!user) return res.status(404).send();
-        res.send(user);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-router.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id;
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowUpdated = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every((update) =>
@@ -75,22 +63,18 @@ router.patch('/users/:id', async (req, res) => {
     if (!isValidOperation)
         return res.status(400).send({ error: 'Invalid updated!' });
     try {
-        const user = await User.findById(_id);
-        if (!user) return res.status(404).send();
-        updates.forEach((update) => (user[update] = req.body[update]));
-        await user.save();
-        res.send(user);
+        updates.forEach((update) => (req.user[update] = req.body[update]));
+        await req.user.save();
+        res.send(req.user);
     } catch (err) {
         res.status(400).send(err);
     }
 });
 
-router.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id;
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(_id);
-        if (!user) return res.status(404).send();
-        res.send(user);
+        await req.user.remove();
+        res.send(req.user);
     } catch (err) {
         res.status(400).send(err);
     }
